d4bc8d430d5c189495a03b69e6e8a5613fc416ce
jpgard <joshua.patrick.gardner@gmail.com>
2021-01-28 20:36:23 -0800

    add requirements.txt

--- analysis/results_plots.R
+++ analysis/results_plots.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(ggthemes)
 library(magrittr)
 library(gridExtra)
+library(magrittr)
 
 german_results = read.csv("/Users/jpgard/Documents/github/fair-robust/results.csv")
 adult_results_sex = read.csv("/Users/jpgard/Documents/github/fair-robust/adult-sex-bs512e1000l1.0-sex-results.csv")
@@ -31,81 +32,101 @@ data_summary <- function(data, varname, groupnames){
 
 
 # Plot for german results
+german_results$train_subset <- forcats::fct_recode(german_results$train_subset, "Union"="all", 
+                    "Minority"="minority", "Majority"="majority")
+german_results$eval_subset <- forcats::fct_recode(german_results$eval_subset, "Union"="all", 
+                                                   "Minority"="minority", "Majority"="majority")
 p_german <- german_results %>% 
   data_summary(varname="loss", 
                groupnames=c("train_subset", "eval_subset")) %>%
-  ggplot(aes(x=train_subset, y=loss, group=eval_subset, col=eval_subset)) +
-  geom_bar(stat="identity", 
-           fill="grey",
-           position=position_dodge(.9),
-           width=0.5,
-           size=1) +
+  ggplot(aes(x=train_subset, y=loss, group=eval_subset, fill=eval_subset)) +
+  geom_col(position = position_dodge2(width = 0.4, preserve = "single"), colour = "black") +
   geom_errorbar(aes(ymin=loss-sd, ymax=loss+sd), width=.2, size=1,
                 position=position_dodge(.9)
                 ) +
-  ggtitle("German Credit Dataset\nSensitive Attribute: Sex\n1 s.d. error bars shown (n = 1000)") +
+  ggtitle("German Credit Dataset\nSensitive Attribute: Sex\n(n = 1,000)") +
   xlab("Training Subset") +
-  scale_color_colorblind(name="Test Subset") +
+  ylab("Loss") + 
+  scale_fill_manual(values=c("#0072B2", "#E69F00", "#56B4E9"), name="Test Subset") +
   guides(color=guide_legend(override.aes=list(fill=NA))) +
-  theme_bw()
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
 p_german
 
 # Plot for adult results (sex)
+adult_results_sex$train_subset <- forcats::fct_recode(adult_results_sex$train_subset, "Union"="all", 
+                                                   "Minority"="minority", "Majority"="majority")
+adult_results_sex$eval_subset <- forcats::fct_recode(adult_results_sex$eval_subset, "Union"="all", 
+                                                  "Minority"="minority", "Majority"="majority")
 p_adult_sex <- adult_results_sex %>%
   data_summary(varname="loss",
                groupnames=c("train_subset", "eval_subset")) %>%
-  ggplot(aes(x=train_subset, y=loss, group=eval_subset, col=eval_subset)) +
-  geom_bar(stat="identity", 
-           fill="grey",
-           position=position_dodge(.9),
-           width=0.5,
-           size=1) +
+  ggplot(aes(x=train_subset, y=loss, group=eval_subset, fill=eval_subset)) +
+  geom_col(position = position_dodge2(width = 0.4, preserve = "single"), colour = "black") +
   geom_errorbar(aes(ymin=loss-sd, ymax=loss+sd), width=.2, size=1,
                 position=position_dodge(.9)
   ) +
-  ggtitle("Adult Dataset\nSensitive Attribute: Sex\n1 s.d. error bars shown (n = 37,000)") +
+  ggtitle("Adult Dataset\nSensitive Attribute: Sex\n(n = 37,000)") +
   xlab("Training Subset") +
-  scale_color_colorblind(name="Test Subset") +
+  ylab("Loss") + 
+  scale_fill_manual(values=c("#0072B2", "#E69F00", "#56B4E9"), name="Test Subset") +
   guides(color=guide_legend(override.aes=list(fill=NA))) +
-  theme_bw()
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
 p_adult_sex
 
 # Plot for adult results (race)
+adult_results_race$train_subset <- forcats::fct_recode(adult_results_race$train_subset, "Union"="all", 
+                                                      "Minority"="minority", "Majority"="majority")
+adult_results_race$eval_subset <- forcats::fct_recode(adult_results_race$eval_subset, "Union"="all", 
+                                                     "Minority"="minority", "Majority"="majority")
 p_adult_race <- adult_results_race %>% data_summary(varname="loss", 
                               groupnames=c("train_subset", "eval_subset")) %>%
-  ggplot(aes(x=train_subset, y=loss, group=eval_subset, col=eval_subset)) +
-  geom_bar(stat="identity", 
-           fill="grey",
-           position=position_dodge(.9),
-           width=0.5,
-           size=1) +
+  ggplot(aes(x=train_subset, y=loss, group=eval_subset, fill=eval_subset)) +
+  geom_col(position = position_dodge2(width = 0.4, preserve = "single"), colour = "black") +
   geom_errorbar(aes(ymin=loss-sd, ymax=loss+sd), width=.2, size=1,
                 position=position_dodge(.9)
   ) +
-  ggtitle("Adult Dataset\nSensitive Attribute: Race\n1 s.d. error bars shown (n = 37,000)") +
+  ggtitle("Adult Dataset\nSensitive Attribute: Race\n(n = 37,000)") +
   xlab("Training Subset") +
-  scale_color_colorblind(name="Test Subset") +
+  ylab("Loss") + 
+  scale_fill_manual(values=c("#0072B2", "#E69F00", "#56B4E9"), name="Test Subset") +
   guides(color=guide_legend(override.aes=list(fill=NA))) +
-  theme_bw()
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
 p_adult_race
 
 # Plot for compas results (race)
+compas_results_race$train_subset <- forcats::fct_recode(compas_results_race$train_subset, "Union"="all", 
+                                                       "Minority"="minority", "Majority"="majority")
+compas_results_race$eval_subset <- forcats::fct_recode(compas_results_race$eval_subset, "Union"="all", 
+                                                      "Minority"="minority", "Majority"="majority")
 p_compas_race <- compas_results_race %>% data_summary(varname="loss", 
                                     groupnames=c("train_subset", "eval_subset")) %>%
-  ggplot(aes(x=train_subset, y=loss, group=eval_subset, col=eval_subset)) +
-  geom_bar(stat="identity", 
-           fill="grey",
-           position=position_dodge(.9),
-           width=0.5,
-           size=1) +
+  ggplot(aes(x=train_subset, y=loss, group=eval_subset, fill=eval_subset)) +
+  geom_col(position = position_dodge2(width = 0.4, preserve = "single"), colour = "black") +
   geom_errorbar(aes(ymin=loss-sd, ymax=loss+sd), width=.2, size=1,
                 position=position_dodge(.9)
   ) +
-  ggtitle("COMPAS Dataset\nSensitive Attribute: Race\n1 s.d. error bars shown (n = 5,200)\nNote: 'Minority' group is LARGER in COMPAS") +
+  ggtitle("COMPAS Dataset\nSensitive Attribute: Race\n(n = 5,200)") +
   xlab("Training Subset") +
-  scale_color_colorblind(name="Test Subset") +
+  ylab("Loss") + 
+  scale_fill_manual(values=c("#0072B2", "#E69F00", "#56B4E9"), name="Test Subset") +
   guides(color=guide_legend(override.aes=list(fill=NA))) +
-  theme_bw()
+  theme_bw() +
+  theme(plot.title=element_text(hjust=0.5),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+labs(caption = "Note: 'Minority' attribute group is larger than 'Majority' in COMPAS.")
 p_compas_race
 
-grid.arrange(p_german, p_compas_race, p_adult_sex, p_adult_race, ncol=2)
+grid.arrange(p_german, p_compas_race, p_adult_sex, p_adult_race, ncol=4)
+library(ggpubr)
+ggarrange(p_german, p_compas_race, p_adult_sex, p_adult_race, nrow=1, common.legend = TRUE, legend="bottom")
+ggsave("./subset_experiments_fairness_datasets.pdf", device="pdf", width=16, height=7)
